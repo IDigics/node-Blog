27e768420251f866afe91aeea0d1e6c6b0e4f922
KhaledCherif <cherifkhaled.tn@gmail.com>
2025-07-15 10:37:48 +0100

    controllersANDservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,17 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { VotesModule } from './votes/votes.module';
 
+import { User } from './users/user.entity';
+import { Post } from './posts/post.entity';
+import { Vote } from './votes/vote.entity';
+import { Comment } from './comments/comment.entity';
+
 @Module({
   imports: [
-  TypeOrmModule.forRoot({
+TypeOrmModule.forRoot({
   type: 'sqlite',
   database: 'db.sqlite',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [User, Post, Comment, Vote],
   synchronize: true,
 }),
 AuthModule, 
--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { VotesService } from './votes.service';
+import { VotesController } from './votes.controller';
 
-@Module({})
+@Module({
+  providers: [VotesService],
+  controllers: [VotesController]
+})
 export class VotesModule {}
