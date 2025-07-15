e2224011bda30370842c897f0c59aed3a730fe90
KhaledCherif <cherifkhaled.tn@gmail.com>
2025-07-15 10:46:40 +0100

    modulesUpdate

--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VotesService } from './votes.service';
 import { VotesController } from './votes.controller';
+import { Vote } from './vote.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Vote])],
   providers: [VotesService],
   controllers: [VotesController]
 })
